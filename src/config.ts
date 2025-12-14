10fb35c95db6c31e141b70ae793cb033324b4b76
“magisk-for-arm” <lin666_666@outlook.com>
2025-12-14 17:57:38 +0800

    none

--- src/config.ts
+++ src/config.ts
@@ -171,7 +171,7 @@ export const siteConfig: SiteConfig = {
 		},
 
 		navbar: {
-			transparentMode: "semifull", // 导航栏透明模式："semi" 半透明加圆角，"full" 完全透明，"semifull" 动态透明
+			transparentMode: "semi", // 导航栏透明模式："semi" 半透明加圆角，"full" 完全透明，"semifull" 动态透明
 		},
 	},
 	toc: {
