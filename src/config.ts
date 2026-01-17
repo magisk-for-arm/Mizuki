3c3d2862cf63a15d47946ff2709fee6fbb24ac03
“magisk-for-arm” <lin666_666@outlook.com>
2026-01-17 19:50:29 +0800

    pic

--- src/config.ts
+++ src/config.ts
@@ -57,7 +57,7 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
 		icon: "assets/home/fix.webp",
 		// 网站Logo图片路径
-		logo: "assets/home/default-logo.png",
+		logo: "assets/home/fix.webp",
 	},
 
 	bangumi: {
