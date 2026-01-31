9a21544ebc9c364aaaf9591cce25683c0665a8c4
“magisk-for-arm” <lin666_666@outlook.com>
2026-01-31 15:17:20 +0800

    fix SiteURl

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "armの小破站",
 	subtitle: "armの小破站",
-	siteURL: "https://vc.520403,xyz/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://vc.520403.xyz/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-07-30", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
